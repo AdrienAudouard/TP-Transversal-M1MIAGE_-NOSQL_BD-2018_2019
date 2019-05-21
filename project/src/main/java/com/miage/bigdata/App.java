e455792de8da420658a9ac546dbe2f6c053f1eb3
Romain Lembo <romainlembo06@gmail.com>
2019-05-21 15:30:09 +0200

    init

---
+++ project/src/main/java/com/miage/bigdata/App.java
@@ -0,0 +1,13 @@
+package com.miage.bigdata;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
