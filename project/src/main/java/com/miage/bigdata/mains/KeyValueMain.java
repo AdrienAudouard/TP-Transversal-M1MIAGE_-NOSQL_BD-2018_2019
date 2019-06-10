bfaf442fe6a5d7fe05772ee9735f0ed3e9ac1ab3
Victor Monsch <victor.monsch@gmail.com>
2019-06-10 07:34:34 +0200

    Code cleanup for KeyValueMain

--- project/src/main/java/com/miage/bigdata/mains/KeyValueMain.java
+++ project/src/main/java/com/miage/bigdata/mains/KeyValueMain.java
@@ -3,18 +3,26 @@ package com.miage.bigdata.mains;
 import com.miage.bigdata.controllers.item.ItemController;
 import com.miage.bigdata.controllers.models.ModelController;
 import com.miage.bigdata.models.keyvalue.FeedbackItem;
+import com.miage.bigdata.models.keyvalue.KeyValueItem;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class KeyValueMain {
+    private static ModelController modelController = new ModelController();
+    private static ItemController<FeedbackItem> feedbackItemController = modelController.getItemController(FeedbackItem.class);
 
     public static void main(String args[]) {
-        ModelController modelController = new ModelController();
-        ItemController<FeedbackItem> feedbackItemController = modelController.getItemController(FeedbackItem.class);
+        deleteAllFeedbacks();
+        createExampleFeedbacks();
+        getAllFeedbacks();
+        updateExampleFeedbacks();
+        getAllFeedbacks();
+    }
 
-        //region Create
-        System.out.println("------------ Create items ------------");
+    private static void createExampleFeedbacks() {
+        System.out.println("------------ Create example feedback items ------------");
 
         FeedbackItem fb1 = new FeedbackItem("product1", "user1", 4.5, "super");
         FeedbackItem fb2 = new FeedbackItem("product1", "user2", 1.0, "déçu");
@@ -23,26 +31,37 @@ public class KeyValueMain {
         FeedbackItem fb4 = new FeedbackItem("product2", "user2", 3.0, "ok sans plus");
 
         System.out.println(feedbackItemController.create(fb1, fb2, fb3, fb4));
-        //endregion
+    }
 
-        //region GetAll
-        System.out.println("------------ Print all items ------------");
+    private static List<FeedbackItem> getAllFeedbacks() {
+        System.out.println("------------ Get all feedback items ------------");
 
         ArrayList<FeedbackItem> readFeedbackItems = new ArrayList<>(feedbackItemController.readAll());
         System.out.println(readFeedbackItems.toString());
 
-        //What were the roles of the lines below ?
-        //feedbackItemController.deleteTable();
-        //feedbackItemController.createTable();
+        return readFeedbackItems;
+    }
 
-        //endregion
+    private static void updateExampleFeedbacks() {
+        System.out.println("------------ Update example feedback items ------------");
 
-        //region DeleteAll
-        System.out.println("------------ Delete all items ------------");
+        FeedbackItem fb1 = new FeedbackItem("product1", "user1", 5.0, "parfait depuis la réponse du support client");
+        FeedbackItem fb2 = new FeedbackItem("product1", "user2", 3.0, "en fait j'aime bien");
 
-        for (FeedbackItem itemToDelete : readFeedbackItems) {
+        FeedbackItem fb3 = new FeedbackItem("product2", "user1", 3.0, "bien reçu finalement juste long");
+        FeedbackItem fb4 = new FeedbackItem("product2", "user2", 1.0, "il s'est cassé au bout d'une semaine");
+
+        System.out.println(feedbackItemController.update(fb1));
+        System.out.println(feedbackItemController.update(fb2));
+        System.out.println(feedbackItemController.update(fb3));
+        System.out.println(feedbackItemController.update(fb4));
+    }
+
+    private static void deleteAllFeedbacks() {
+        System.out.println("------------ Delete all feedback items ------------");
+
+        for (FeedbackItem itemToDelete : getAllFeedbacks()) {
             System.out.print(feedbackItemController.delete(itemToDelete.getFullKey()) + " ");
         }
-        //endregion
     }
 }
